26988424bcf7aadfa5b1fdd2e0f9d6df35a9bd7a
pedro <phr-pedro@hotmail.com>
2025-04-29 20:58:07 -0300

    Correções de queries

--- src/main/kotlin/com/pedrorodrigues/desafiofinal/repository/ProjectCustomTaskRepository.kt
+++ src/main/kotlin/com/pedrorodrigues/desafiofinal/repository/ProjectCustomTaskRepository.kt
@@ -8,39 +8,40 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface ProjectCustomTaskRepository : MongoRepository<Project, String> {
+
     @Aggregation(
         pipeline = [
-            "{\"\\\$match\": {\"_id\": \"?0\"}}",
-            "{\"\\\$unwind\": \"\\\$tasks\"}",
-            "{\"\\\$replaceRoot\": {\"newRoot\": \"\\\$tasks\"}}"
+            "{\"\$match\": {\"_id\": \"?0\"}}",
+            "{\"\$unwind\": \"\$tasks\"}",
+            "{\"\$replaceRoot\": {\"newRoot\": \"\$tasks\"}}"
         ]
     )
     fun findAllByProjectId(projectId: String): List<Task>
 
     @Aggregation(
         pipeline = [
-            "{\"\\\$match\": {\"_id\": \"?0\"}}",
-            "{\"\\\$unwind\": \"\\\$tasks\"}",
-            "{\"\\\$match\": {\"tasks.id\": \"?1\"}}",
-            "{\"\\\$replaceRoot\": {\"newRoot\": \"\\\$tasks\"}}"
+            "{\"\$match\": {\"_id\": \"?0\"}}",
+            "{\"\$unwind\": \"\$tasks\"}",
+            "{\"\$match\": {\"tasks.id\": \"?1\"}}",
+            "{\"\$replaceRoot\": {\"newRoot\": \"\$tasks\"}}"
         ]
     )
     fun findByProjectIdAndTaskId(projectId: String, taskId: String): Task?
 
     @Aggregation(
         pipeline = [
-            "{\"\\\$match\": {\"_id\": \"?0\"}}",
-            "{\"\\\$unwind\": \"\\\$tasks\"}",
-            "{\"\\\$match\": {\"tasks.title\": {\"\\\$regex\": \"?1\", \"\\\$options\": \"i\"}}}",
-            "{\"\\\$replaceRoot\": {\"newRoot\": \"\\\$tasks\"}}"
+            "{\"\$match\": {\"_id\": \"?0\"}}",
+            "{\"\$unwind\": \"\$tasks\"}",
+            "{\"\$match\": {\"tasks.title\": {\"\$regex\": \"?1\", \"\$options\": \"i\"}}}",
+            "{\"\$replaceRoot\": {\"newRoot\": \"\$tasks\"}}"
         ]
     )
     fun findByProjectIdAndTitleContaining(projectId: String, title: String): List<Task>
 
     @Aggregation(
         pipeline = [
-            "{\"\\\$match\": {\"_id\": \"?0\"}}",
-            "{\"\\\$project\": {\"_id\": 0, \"taskCount\": {\"\\\$size\": \"\\\$tasks\"}}}"
+            "{\"\$match\": {\"_id\": \"?0\"}}",
+            "{\"\$project\": {\"_id\": 0, \"taskCount\": {\"\$size\": \"\$tasks\"}}}"
         ]
     )
     fun countTasksByProjectId(projectId: String): Long
